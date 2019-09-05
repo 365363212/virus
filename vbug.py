70d011ba035cdf4e7fc970e552345044eb3146e1
365363212 <35191246+365363212@users.noreply.github.com>
2019-09-06 01:58:37 +0700

    update vbug.py
    receh

--- vbug.py
+++ vbug.py
@@ -3,27 +3,7 @@ print '\x1b[1;32mSudah punya ID dan Password nya?'
 print '\x1b[1;32mSilahkan Login '
 import os, sys
 
-def wa():
-    os.system('xdg-open https://api.whatsapp.com/send?phone=6282278198738&text=Assalamualaikum')
-
-
-def restart():
-    ngulang = sys.executable
-    os.execl(ngulang, ngulang, *sys.argv)
-
-
-user = raw_input('ID: ')
-import getpass
-sandi = raw_input('Password: ')
-if sandi == 'ID' and user == 'Termux':
-    print 'Anda Telah Login'
-    sys.exit
-else:
-    print 'Login GAGAL, Silahkan hubungi ADMIN'
-    wa()
-    restart()
-
-import os, sys, time
+defimport os, sys, time
 R = '\x1b[1;31m'
 N = '\x1b[0m'
 Y = '\x1b[1;33m'
